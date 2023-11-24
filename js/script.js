7d2d17a52a45c9d144617d3c460059683d50e9a9
AlessandroSemeraro <alesemeraro1994@gmail.com>
2023-11-24 16:59:56 +0100

    Added array list and for in

--- js/script.js
+++ js/script.js
@@ -1,6 +1,45 @@
 //creare un array di oggetti per rappresentare i membri del team//
+
 const teamList = [
-    {name_and_lastname: 'Wayne Barnett', 
-    role: 'Founder & CEO', 
-    picture:}
-]
+    {nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    picture: 'wayne-barnett-founder-ceo.jpg'
+},
+    
+    {nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    picture: 'angela-caroll-chief-editor.jpg'
+},
+
+    {nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    picture: 'walter-gordon-office-manager.jpg'
+},
+
+    {nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    picture: 'angela-lopez-social-media-manager.jpg'
+},
+  
+    {nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    picture: 'scott-estrada-developer.jpg'
+},
+
+    {nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    picture: 'barbara-ramos-graphic-designer.jpg'
+}
+] 
+
+for (let key in teamList){
+    console.log(teamList[key])
+}
+
+    
+
+    
+
+    
+   
+
